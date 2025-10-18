ce14fe3d39db7f7118632a59261b952cf99774ac
jpedro-cf <jpedroswc453@gmail.com>
2025-10-17 22:15:53 -0300

    refactor: remove unused method

--- src/main/java/com/github/jpedrocf/trabalhowebdev/TarefaRepository.java
+++ src/main/java/com/github/jpedrocf/trabalhowebdev/TarefaRepository.java
@@ -2,8 +2,5 @@ package com.github.jpedrocf.trabalhowebdev;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface TarefaRepository extends JpaRepository<Tarefa,Long> {
-    Optional<Tarefa> findByResponsavel(String responsavel);
 }
